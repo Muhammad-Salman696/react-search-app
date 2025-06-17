c69e96dfed2173b6728a30d916bbb89a2b538d0a
Muhammad Salman <salmancloud696@gmail.com>
2025-06-17 08:57:58 +0200

    Using of sample data for async functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,6 @@
 import * as React from 'react';
 
-const isLoggedIn = true;
-
-const App = () => {
-
-  const useStorageState = (key, initialState) =>{
-    const [value, setValue] = React.useState(
-      localStorage.getItem(key) || initialState
-    );
-
-    React.useEffect(() =>{
-    localStorage.setItem(key , value);
-    }, [key, value]);
-
-    return [value, setValue]
-  };
-
-   const [searchValue, setSearchValue] = useStorageState('search', 'React');
-
-  
-
-  const handleSearch = (event) =>{
-    setSearchValue(event.target.value);
-  }
-
-  const stories = [
+const initialStories = [
     {
     title: 'React',
     url: 'https://react.dev/',
@@ -42,51 +18,139 @@ const App = () => {
     objectID: 1,
     },
   ];
+const getAsyncStories = ()=>
+  new Promise((resolve, reject)=>
+    setTimeout(()=>{
+      resolve({data : {stories : initialStories}})
+      
+    },2000)
+  )
 
-  const matchStory = stories.filter((story) => 
+  const storiesReducer = (state, action) =>{
+    switch (action.type) {
+        case 'STORIES_FETCH_INIT':
+          return {
+          ...state,
+          isLoading: true,
+          isError: false,
+          };
+        case 'STORIES_FETCH_SUCCESS':
+          return {
+          ...state,
+          isLoading: false,
+          isError: false,
+          data: action.payload,
+          };
+        case 'STORIES_FETCH_FAILURE':
+          return {
+          ...state,
+          isLoading: false,
+          isError: true,
+          };
+        case 'REMOVE_STORY':
+          return {
+          ...state,
+          data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+          ),
+          };
+        default:
+          throw new Error();
+    }
+
+  }
+
+const App = () => {
+
+  const useStorageState = (key, initialState) =>{
+    const [value, setValue] = React.useState(
+      localStorage.getItem(key) || initialState
+    );
+
+    React.useEffect(() =>{
+    localStorage.setItem(key , value);
+    }, [key, value]);
+
+    return [value, setValue]
+  };
+
+  const [searchValue, setSearchValue] = useStorageState('search', 'React');
+  const [stories, dispatchStories] = React.useReducer(storiesReducer ,{data : [], isLoading : false, isError : false}); 
+
+  React.useEffect(()=>{
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    getAsyncStories().then(result => 
+      { dispatchStories({
+        type : "STORIES_FETCH_SUCCESS",
+        payload : result.data.stories,
+      });
+      }).catch(()=> dispatchStories({ type: 'STORIES_FETCH_FAILURE' }))
+  },[])
+
+  const handleSearch = (event) =>{
+    setSearchValue(event.target.value);
+  }
+
+  const matchStory = stories.data.filter((story) => 
     story.title.toLowerCase().includes(searchValue.toLowerCase())
   ) ;
+
+  const handleRemoveStory = (item) => {
+    dispatchStories({
+    type: 'REMOVE_STORY',
+    payload: item,
+    });
+  };
   
   return(
     <>
     
       <div>
-        <List list={matchStory}/>
-        <Search search= {searchValue}  onSearch= {handleSearch}/>
+        {stories.isError && <p>Something went wrong ...</p>}
+        {(stories.isLoading) ? (<p>Loading...</p>)
+        :
+        (<List list={matchStory} handleDelete={handleRemoveStory}/>)
+        }
+        
+        <InputWithLabel id= "search" value= {searchValue} onInputChange= {handleSearch}>
+        Search : 
+        </InputWithLabel> 
       </div>
     </>
   )
 }
 
-const List = ({list})=> 
+const List = ({list, handleDelete})=> 
     <>
-      {isLoggedIn ? 
-          <ul>
-            {list.map(({objectID, ...item})=>
-              <Item key={objectID} {...item}/>
-            )}
-          </ul> : "Plz log in"}
-         
+      <ul>
+          {list.map((item)=>
+              <Item key={item.objectID} item={item} handleDelete={handleDelete}/>
+          )}
+        </ul>    
     </>
 
-const Item = ({title, url, author, num_comments, points})=>
+const Item = ({item, handleDelete})=>{
+  
+  return(
   <>
     <li>
-        <span><a  href={url}> {title} </a></span>
-        <span><p>Author : {author}</p></span>
-        <span>No of comments : {num_comments}</span>
-        <span><p>Points : {points}</p></span>
+        <span><a  href={item.url}> {item.title} </a></span>
+        <span><p>Author : {item.author}</p></span>
+        <span>No of comments : {item.num_comments}</span>
+        <span><p>Points : {item.points}</p></span>
+        <span><button onClick={()=>handleDelete(item)}>Delete Story</button></span>
     </li>
   </>
+  )
+}
 
-
-const Search = ({search , onSearch}) => 
+const InputWithLabel = ({id, children, value, onInputChange, type = "text"}) => 
   (
     <>
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" value={search} onChange={onSearch}/>
+      <label htmlFor={id} > {children} </label>
+      <input type={type} id={id} value={value} onChange={onInputChange}/>
     </>
   )  
-
 
 export default App;
